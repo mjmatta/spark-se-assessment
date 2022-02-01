9723611b24ea7914502c752c89dede70952771be
mjmatta <90805572+mjmatta@users.noreply.github.com>
2022-02-01 14:17:11 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@
 
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.ProductionConfig"
-#flask db init
-#flask db migrate
+flask db init
+flask db migrate
 flask db upgrade
 #flask run --host=0.0.0.0 --port=5000
 flask run
